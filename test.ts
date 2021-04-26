6aa0de29b2a8fb1bcff1b918a74b2194a2c88eae
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-04-26 12:31:53 +0200

    asfasf

---
+++ test.ts
@@ -0,0 +1,15 @@
+interface NamedPerson {
+    firstName: string;
+    age?: number;
+    [propName: string]: any;
+  }
+
+  const person: NamedPerson = {
+    firstName: 'Robert',
+    lastName: 'Molina',
+    age: 24,
+    hobbies: ['Programming', 'Cooking'],
+    greet(lastName: string) {
+      console.log(`Hi, I am ${this.firstName} ${lastName}!`);
+    }
+  }
